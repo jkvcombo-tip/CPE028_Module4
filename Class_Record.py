e908a9d2c07ed78d9f31631078fbcca1a09dbc38
jkvcombo-tip <qjkvcombo@tip.edu.ph>
2023-02-20 10:14:25 +0000

    Add Class_Record/Modify Student.py

---
+++ Class_Record.py
@@ -0,0 +1,6 @@
+import Student
+
+p1=Student.Student("John Kenneth Combo",23)
+print(p1)
+print("Name: ", p1.name)
+print("Age: ", p1.age)
--- Student.py
+++ Student.py
@@ -6,10 +6,3 @@ class Student:
     def __str__(self):
         return f"{self.name}({self.age})"
 
-p1 = Student("John Kenneth Combo", 23)
-p2 = Student("Claudith Perlas", 23)
-print(p1)
-print("Name: ", p1.name)
-print("Age: ", p1.age)
-print("Name: ", p2.name)
-print("Age: ", p2.age)
